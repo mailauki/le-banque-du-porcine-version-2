d7a687dd1028d88e85596e24f739e23415b1b6ad
Julie Evans <juliemevans17@gmail.com>
2022-10-08 15:26:56 -0500

    Added image and priority to ItemEl

--- components/ItemEl.js
+++ components/ItemEl.js
@@ -1,10 +1,23 @@
 import styles from '../styles/Home.module.css'
+import LooksOneIcon from '@mui/icons-material/LooksOne';
+import LooksTwoIcon from '@mui/icons-material/LooksTwo';
+import Looks3Icon from '@mui/icons-material/Looks3';
 
 export default function ItemEl({ item }) {
+  const priorityIcons = {
+    1: <LooksOneIcon color="error" />,
+    2: <LooksTwoIcon color="warning" />,
+    3: <Looks3Icon color="success" />,
+  }
+
   return (
     <div className={styles.card}>
       <img src={item.image} alt={item.name} />
       <p>{item.name} - ${item.price.toFixed(2)}</p>
+      <div style={{ display: "flex", alignContent: "center" }}>
+        <p>Priority: </p>
+        <p>{priorityIcons[item.priority]}</p>
+      </div>
     </div>
   )
 }
